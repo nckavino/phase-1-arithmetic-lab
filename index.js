8476c5f08f4dfc70cd0e5f6e5b80d06b698edd76
NicholasAvino <nckavino0903@gmail.com>
2022-08-30 22:38:53 -0400

    commit tree 2c3de5d7ec37a8a3cc04511c2e5f5d6385e91733

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 25;
+let num4 = 7;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20);
